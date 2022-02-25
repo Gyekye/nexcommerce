872728dc1aa8d9dc9af2f5b1caa0b954430de134
Gyekye <mrgyekye740@gmail.com>
2022-02-25 19:42:17 +0000

    deps: add react-firebaseui and react-firebase-hooks for authentication.

---
+++ lib/backend/firebase/config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import {getAnalytics} from 'firebase/analytics';
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyDEBdnsaayI6PwWu6c1M0KoVBtTTZQyEQ8',
+	authDomain: 'nexcommerce-b7d0f.firebaseapp.com',
+	projectId: 'nexcommerce-b7d0f',
+	storageBucket: 'nexcommerce-b7d0f.appspot.com',
+	messagingSenderId: '137542996011',
+	appId: '1:137542996011:web:63d80d152aa601d7939a6e',
+	measurementId: 'G-4F7E5HD889'
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
