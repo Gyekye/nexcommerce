b852d389eb4ae9290de5de52a77796cad58316cb
Jeron <norejudeisa@gmail.com>
2022-03-23 22:26:43 +0000

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,8 @@
-import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import Layout from '../components/layout'
+import type { AppProps } from 'next/app'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './features/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './core/**/*.{js,ts,jsx,tsx}',
+    './features/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
