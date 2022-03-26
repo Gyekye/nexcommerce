0cfbad9b6bcde918718945a48575e91de416a680
Jeron <norejudeisa@gmail.com>
2022-03-26 23:20:38 +0000

    implement marque and footer section

--- components/Marque/Marque.tsx
+++ components/Marque/Marque.tsx
@@ -1,20 +1,128 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Marque = (): JSX.Element => {
   return (
-    <div className="h-80 bg-black w-full overflow-hidden">
-      <div className="flex">
+    <div className="h-[23rem] md:h-[24rem] lg:h-[25rem] bg-black w-full overflow-hidden">
+      <div className="flex w-full h-full animate-marque">
         <Link href="/">
-          <a>somewhere</a>
+          <a
+            className="h-full flex-shrink-0 w-[30rem]
+          relative "
+          >
+            <Image
+              src="/assets/hat.webp"
+              alt="image"
+              layout="fill"
+              objectFit="cover"
+              className="scale-95"
+            />
+            <h3 className="text-3xl bg-white absolute p-2 right-0 top-1/2 -translate-y-1/2">
+              Black Beanie
+            </h3>
+          </a>
         </Link>
         <Link href="/">
-          <a>somewhere</a>
+          <a
+            className="h-full flex-shrink-0 w-[30rem]
+          relative "
+          >
+            <Image
+              src="/assets/t-shirt.webp"
+              alt="image"
+              layout="fill"
+              objectFit="cover"
+              className="scale-95"
+            />
+            <h3 className="text-3xl bg-white absolute p-2 right-0 top-1/2 -translate-y-1/2">
+              Black Beanie
+            </h3>
+          </a>
         </Link>
         <Link href="/">
-          <a>somewhere</a>
+          <a
+            className="h-full flex-shrink-0 w-[30rem]
+          relative "
+          >
+            <Image
+              src="/assets/jacket.webp"
+              alt="image"
+              layout="fill"
+              objectFit="cover"
+              className="scale-95"
+            />
+            <h3 className="text-3xl bg-white absolute p-2 right-0 top-1/2 -translate-y-1/2">
+              Black Beanie
+            </h3>
+          </a>
         </Link>
         <Link href="/">
-          <a>somewhere</a>
+          <a
+            className="h-full flex-shrink-0 w-[30rem]
+          relative "
+          >
+            <Image
+              src="/assets/mask.webp"
+              alt="image"
+              layout="fill"
+              objectFit="cover"
+              className="scale-95"
+            />
+            <h3 className="text-3xl bg-white absolute p-2 right-0 top-1/2 -translate-y-1/2">
+              Black Beanie
+            </h3>
+          </a>
+        </Link>
+        <Link href="/">
+          <a
+            className="h-full flex-shrink-0 w-[30rem]
+          relative "
+          >
+            <Image
+              src="/assets/hat.webp"
+              alt="image"
+              layout="fill"
+              objectFit="cover"
+              className="scale-95"
+            />
+            <h3 className="text-3xl bg-white absolute p-2 right-0 top-1/2 -translate-y-1/2">
+              Black Beanie
+            </h3>
+          </a>
+        </Link>
+        <Link href="/">
+          <a
+            className="h-full flex-shrink-0 w-[30rem]
+          relative "
+          >
+            <Image
+              src="/assets/t-shirt.webp"
+              alt="image"
+              layout="fill"
+              objectFit="cover"
+              className="scale-95"
+            />
+            <h3 className="text-3xl bg-white absolute p-2 right-0 top-1/2 -translate-y-1/2">
+              Black Beanie
+            </h3>
+          </a>
+        </Link>
+        <Link href="/">
+          <a
+            className="h-full flex-shrink-0 w-[30rem]
+          relative "
+          >
+            <Image
+              src="/assets/jacket.webp"
+              alt="image"
+              layout="fill"
+              objectFit="cover"
+              className="scale-95"
+            />
+            <h3 className="text-3xl bg-white absolute p-2 right-0 top-1/2 -translate-y-1/2">
+              Black Beanie
+            </h3>
+          </a>
         </Link>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './core/**/*.{js,ts,jsx,tsx}',
-    './features/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./core/**/*.{js,ts,jsx,tsx}",
+    "./features/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        marque: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-500px)" },
+        },
+      },
+      animation: {
+        marque: "marque 20s linear infinite forwards",
+      },
+    },
   },
   plugins: [],
-}
+};
